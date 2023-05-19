280bedc1dd7a26317f5ef224f84e4e60c27ecac0
VictoriaPerezG <victoria.perez@est.fi.uncoma.edu.ar>
2023-05-19 12:08:59 -0300

    form timer

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -12,10 +12,10 @@
     <!-- Bootstrap icons -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
 
-    <link rel="stylesheet" href="{!! asset('css/styles.css') !!}">
-    <link rel="stylesheet" href="{!!asset('css/pagination.css')!!}">
-    <link rel="stylesheet" href="{!!asset('css/estilosTabla.css')!!}">
-    <link rel="stylesheet" href="{!!asset('css/home.css')!!}">
+    <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/pagination.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/estilosTabla.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/home.css') }}">
 
     <style>
         .nav-pills .nav-link.active,
