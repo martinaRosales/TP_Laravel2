983247533accfd17b3369e387540db03eb415ca1
Gonzalo Parra <gonzzaparra@gmail.com>
2023-05-21 23:36:40 -0300

    Avance

---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+
+class RoleSeeder extends Seeder {
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void {
+        //
+        $rol1 = Role::create(['name' => 'Admin']);
+        $rol2 = Role::create(['name' => 'Juez']);
+        $rol3 = Role::create(['name' => 'Competidor']);
+
+        // Creo un permiso y se lo asigno a un solo rol
+        Permission::create(['name' => 'ver competidores inscriptos'])->assignRole($rol1);
+        // Creo un permiso y se lo asigno a dos roles (admin y juez)
+        Permission::create(['name' => 'ver reloj'])->syncRoles([$rol1, $rol2]);
+        Permission::create(['name' => 'ver form inscripcion'])->syncRoles([$rol3]);
+
+    }
+
+}
